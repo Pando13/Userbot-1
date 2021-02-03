f8bf8414ad82f2218ca91270e2e4a127b8356176
Leoatomic <69369111+Leoatomic@users.noreply.github.com>
2021-02-03 08:21:18 +0100

    Update covid.py

--- userbot/plugins/covid.py
+++ userbot/plugins/covid.py
@@ -15,16 +15,16 @@ async def _(event):
     country_data = covid.get_status_by_country_name(country)
     deaths = covid.get_total_deaths()
     if country_data:
-        output_text =  f"**Confermati**   :   {country_data['confirmed']}\n"
-        output_text += f"**Attuali**           :   {country_data['active']}\n"
-        output_text += f"**Morti**          :   {country_data['deaths']}\n"
-        output_text += f"**Ricoverati**    :   {country_data['recovered']}\n\n"        
+        output_text =  f"**Confermati** :   {country_data['confirmed']}\n"
+        output_text += f"**Attuali**    :   {country_data['active']}\n"
+        output_text += f"**Morti**      :   {country_data['deaths']}\n"
+        output_text += f"**Ricoverati** :   {country_data['recovered']}\n\n"        
         output_text += f"---------TOTALI---------\n\n"                
-        output_text += f"**Morti**           :   {covid.get_total_deaths()}\n"
-        output_text += f"**Ricoverati**     :   {covid.get_total_recovered()}\n"
-        output_text += f"**Confermati**     :   {covid.get_total_confirmed_cases()}\n"
-        output_text += f"**Attuali**            :   {covid.get_total_active_cases()}\n\n"
-        output_text += ("**Aggiornato :  "f"{datetime.utcfromtimestamp(country_data['last_update'] // 1000).strftime('%H:%M')}[GMT]\n")
+        output_text += f"**Morti**      :   {covid.get_total_deaths()}\n"
+        output_text += f"**Ricoverati** :   {covid.get_total_recovered()}\n"
+        output_text += f"**Confermati** :   {covid.get_total_confirmed_cases()}\n"
+        output_text += f"**Attuali**    :   {covid.get_total_active_cases()}\n\n"
+        output_text += ("**Aggiornato** :  "f"{datetime.utcfromtimestamp(country_data['last_update'] // 1000).strftime('%H:%M')}[GMT]\n")
     else:
         output_text = "**Invalid Country name**"
     await event.edit(f"**Corona Virus Info in** {country}:\n\n{output_text}")
