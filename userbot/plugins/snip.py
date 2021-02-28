5af842585df4bca57ccc8c47ff787b729fde494c
Leoatomic <69369111+Leoatomic@users.noreply.github.com>
2021-02-28 16:32:54 +0100

    Update snip.py

--- userbot/plugins/snip.py
+++ userbot/plugins/snip.py
@@ -70,7 +70,7 @@ async def on_snip_save(event):
                 snip['hash'] = media.access_hash
                 snip['fr'] = media.file_reference
         add_snip(name, snip['text'], snip['type'], snip.get('id'), snip.get('hash'), snip.get('fr'))
-        await event.edit("snip {name} salvato. ottienilo con .{name}".format(name=name))
+        await event.edit("**Comando {name} salvato ✅. ottienilo con .{name}**".format(name=name))
     else:
         await event.edit("Reply to a message with `snips keyword` to save the snip")
 
@@ -92,7 +92,7 @@ async def on_snip_list(event):
                 out_file,
                 force_document=True,
                 allow_cache=False,
-                caption="Snips disponibili",
+                caption="Comandi disponibili",
                 reply_to=event
             )
             await event.delete()
@@ -104,4 +104,4 @@ async def on_snip_list(event):
 async def on_snip_delete(event):
     name = event.pattern_match.group(1)
     remove_snip(name)
-    await event.edit("snip #{} eliminato".format(name))
+    await event.edit("**Comando #{} eliminato ✅.**".format(name))
