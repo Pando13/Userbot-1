d806010bdf5f58e64239624a1990f5aade3c8bc5
Leoatomic <69369111+Leoatomic@users.noreply.github.com>
2021-03-14 16:36:40 +0100

    Update fiko.py

--- userbot/plugins/fiko.py
+++ userbot/plugins/fiko.py
@@ -1,24 +1,24 @@
 #by @leoatomic
 """Commands:
-.figo"""
+.fika"""
 
 import asyncio
 from telethon import events
 from userbot import bot
 from userbot.system import dev_cmd
 
-@bot.on(dev_cmd(pattern="figo", outgoing=True))
+@bot.on(dev_cmd(pattern="fika", outgoing=True))
 async def _(event):
     if event.fwd_from:
         return
     animation_interval = 0.1
     animation_ttl = range(0, 101)
     #input_str = event.pattern_match.group(1)
-    #if input_str == "figo":
-    await event.edit("figo")
+    #if input_str == "fika":
+    await event.edit("fika")
     animation_chars = [
 
-            "[𝙇𝙚𝙤⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣](tg://user?id=845549379) è veramente molto fiko 🦾🏻 perché ha l'**Userbot**! ⚙️",
+            "[𝙇𝙚𝙤⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣](tg://user?id=845549379) è **super fiko** 🦸‍♂ perché ha l'**Userbot**! 🪐",
      
         ]
 
