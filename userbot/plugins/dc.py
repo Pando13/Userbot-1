cbf11751736e15bf96644521dfeb1c3a6fe93a74
Leoatomic <69369111+Leoatomic@users.noreply.github.com>
2021-04-03 19:11:48 +0200

    Update dc.py

--- userbot/plugins/dc.py
+++ userbot/plugins/dc.py
@@ -31,62 +31,10 @@ async def _(event):
     if event.fwd_from:
         return    
     try:
-        dc_id, location = get_input_location(replied_user.profile_photo)
+        dc_id, location = get_input_location(replied_user.photo)
     except Exception as e:
         dc_id = "Mi serve una foto profilo per controllare il **DC**"
         location = str(e)
     await event.edit(f"**🌍 DC**: {dc_id}")
     
-    async def get_full_user(event):
-    if event.reply_to_msg_id:
-        previous_message = await event.get_reply_message()
-        if previous_message.forward:
-            replied_user = await event.client(
-                GetFullUserRequest(
-                    previous_message.forward.from_id or previous_message.forward.channel_id
-                )
-            )
-            return replied_user, None
-        else:
-            replied_user = await event.client(
-                GetFullUserRequest(
-                    previous_message.from_id
-                )
-            )
-            return replied_user, None
-    else:
-        input_str = None
-        try:
-            input_str = event.pattern_match.group(1)
-        except IndexError as e:
-            return None, e
-        if event.message.entities is not None:
-            mention_entity = event.message.entities
-            probable_user_mention_entity = mention_entity[0]
-            if isinstance(probable_user_mention_entity, MessageEntityMentionName):
-                user_id = probable_user_mention_entity.user_id
-                replied_user = await event.client(GetFullUserRequest(user_id))
-                return replied_user, None
-            else:
-                try:
-                    user_object = await event.client.get_entity(input_str)
-                    user_id = user_object.id
-                    replied_user = await event.client(GetFullUserRequest(user_id))
-                    return replied_user, None
-                except Exception as e:
-                    return None, e
-        elif event.is_private:
-            try:
-                user_id = event.chat_id
-                replied_user = await event.client(GetFullUserRequest(user_id))
-                return replied_user, None
-            except Exception as e:
-                return None, e
-        else:
-            try:
-                user_object = await event.client.get_entity(int(input_str))
-                user_id = user_object.id
-                replied_user = await event.client(GetFullUserRequest(user_id))
-                return replied_user, None
-            except Exception as e:
-                return None, e
+ 
