6227c9485767313fd10d1fb3c45169932c691427
Leoatomic <69369111+Leoatomic@users.noreply.github.com>
2021-04-03 20:36:10 +0200

    Update dc.py

--- userbot/plugins/dc.py
+++ userbot/plugins/dc.py
@@ -26,15 +26,110 @@ from telethon.tl.custom import Dialog
 from userbot import CMD_HELP, bot, ALIVE_NAME
 from userbot.system import dev_cmd, register, errors_handler
 
-@bot.on(dev_cmd("dc"))
+
+    
+ 
+
+
+
+@bot.on(dev_cmd("info ?(.*)"))
 async def _(event):
     if event.fwd_from:
-        return    
+        return
+    replied_user, error_i_a = await get_full_user(event)
+    if replied_user is None:
+        await event.edit(str(error_i_a))
+        return False
+    replied_user_profile_photos = await bot(GetUserPhotosRequest(
+        user_id=replied_user.user.id,
+        offset=30,
+        max_id=0,
+        limit=70
+    ))
+    replied_user_profile_photos_count = "NaN"
     try:
-        dc_id, location = get_input_location(replied_user.photo)
+        replied_user_profile_photos_count = replied_user_profile_photos.count
+    except AttributeError as e:
+        pass
+    
+    try:
+        dc_id, location = get_input_location(replied_user.profile_photo)
     except Exception as e:
         dc_id = "Mi serve una foto profilo per controllare il **DC**"
         location = str(e)
-    await event.edit(f"**🌍 DC**: {dc_id}")
-    
- 
+    caption = """
+🌍 **DC**:: {}
+
+""".format(
+        
+        dc_id,
+        
+    )
+    message_id_to_reply = event.message.reply_to_msg_id
+    if not message_id_to_reply:
+        message_id_to_reply = event.message.id
+    await bot.send_message(
+        event.chat_id,
+        caption,
+        reply_to=message_id_to_reply,
+        
+        file=replied_user.profile_photo,
+        force_document=False,
+        silent=True
+    )
+    await event.delete()
+
+
+async def get_full_user(event):
+    if event.reply_to_msg_id:
+        previous_message = await event.get_reply_message()
+        if previous_message.forward:
+            replied_user = await event.client(
+                GetFullUserRequest(
+                    previous_message.forward.from_id or previous_message.forward.channel_id
+                )
+            )
+            return replied_user, None
+        else:
+            replied_user = await event.client(
+                GetFullUserRequest(
+                    previous_message.from_id
+                )
+            )
+            return replied_user, None
+    else:
+        input_str = None
+        try:
+            input_str = event.pattern_match.group(1)
+        except IndexError as e:
+            return None, e
+        if event.message.entities is not None:
+            mention_entity = event.message.entities
+            probable_user_mention_entity = mention_entity[0]
+            if isinstance(probable_user_mention_entity, MessageEntityMentionName):
+                user_id = probable_user_mention_entity.user_id
+                replied_user = await event.client(GetFullUserRequest(user_id))
+                return replied_user, None
+            else:
+                try:
+                    user_object = await event.client.get_entity(input_str)
+                    user_id = user_object.id
+                    replied_user = await event.client(GetFullUserRequest(user_id))
+                    return replied_user, None
+                except Exception as e:
+                    return None, e
+        elif event.is_private:
+            try:
+                user_id = event.chat_id
+                replied_user = await event.client(GetFullUserRequest(user_id))
+                return replied_user, None
+            except Exception as e:
+                return None, e
+        else:
+            try:
+                user_object = await event.client.get_entity(int(input_str))
+                user_id = user_object.id
+                replied_user = await event.client(GetFullUserRequest(user_id))
+                return replied_user, None
+            except Exception as e:
+                return None, e
