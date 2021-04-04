1f11cf7892ff90b288ef6e820aa2bea674206201
Leoatomic <69369111+Leoatomic@users.noreply.github.com>
2021-04-04 15:49:44 +0200

    Update fiko.py

--- userbot/plugins/fiko.py
+++ userbot/plugins/fiko.py
@@ -11,17 +11,4 @@ from userbot.system import dev_cmd
 async def _(event):
     if event.fwd_from:
         return
-    animation_interval = 0.1
-    animation_ttl = range(0, 101)
-    #input_str = event.pattern_match.group(1)
-    #if input_str == "fika":
-    await event.edit("fika")
-    animation_chars = [
-
-            "[𝙇𝙚𝙤⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣](tg://user?id=845549379) è veramente molto fiko 🦾 perché ha l'**Userbot**! 🪐",
-     
-        ]
-
-    for i in animation_ttl:
-            await asyncio.sleep(animation_interval)
-            await event.edit(animation_chars[i % 10])
+    await event.edit("[𝙇𝙚𝙤⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣](tg://user?id=845549379) è veramente molto fiko 🦾 perché ha l'**Userbot**! 🪐")
