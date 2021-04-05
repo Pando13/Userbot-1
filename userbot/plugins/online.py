d0237949bbec21237308f72bf1bd1df34a52acd2
Leoatomic <69369111+Leoatomic@users.noreply.github.com>
2021-04-05 22:11:49 +0200

    Update online.py

--- userbot/plugins/online.py
+++ userbot/plugins/online.py
@@ -3,16 +3,16 @@ import asyncio
 from telethon import events
 from userbot import bot
 from userbot.system import dev_cmd
-
-
+import time
+from . import StartTime, get_readable_time
 
 @bot.on(dev_cmd(pattern="alive", outgoing=True))
 async def _(event):
     if event.fwd_from:
         return
- 
-    await event.edit(f"✅ **Userbot Online** ✅\n\n • 🗃 **Database:** `Working` \n • 🪐 **AtomicUserbot Version:** `0.66` \n • 🐍 **Python Version:** `3.9.2`\n • 📚 **Telethon Version:** `1.21.1`")
+    uptime = await get_readable_time((time.time() - StartTime))
+    await event.edit(f"✅ **Userbot Online** ✅\n\n • 🗃 **Database:** `Working` \n • 🪐 **AtomicUserbot Version:** `0.66` \n • 🐍 **Python Version:** `3.9.2`\n • 📚 **Telethon Version:** `1.21.1` \n • ⏳ **UpTime:** `{uptime}\n")
 
-    
+
     
 
