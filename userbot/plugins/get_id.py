6b4da6c1e94a515b25c58330fd9b9eef93e08edb
Leoatomic <69369111+Leoatomic@users.noreply.github.com>
2021-04-05 21:28:24 +0200

    Update get_id.py

--- userbot/plugins/get_id.py
+++ userbot/plugins/get_id.py
@@ -6,17 +6,43 @@ from userbot import bot
 from userbot.system import dev_cmd
 
 
-@bot.on(dev_cmd("get_id"))
+@bot.on(dev_cmd("get_id"))      
 async def _(event):
     if event.fwd_from:
         return
-    if event.reply_to_msg_id:
-        chat = await event.get_input_chat()
+    input_str = event.pattern_match.group(2)
+    if input_str:
+        try:
+            p = await event.client.get_entity(input_str)
+        except Exception as e:
+            return await edit_delete(event, f"`{str(e)}`", 5)
+        try:
+            if p.first_name:
+                return await edit_or_reply(
+                    event, f"**Utente:** `{input_str}` \n **ID:** `{p.id}`"
+                )
+        except Exception:
+            try:
+                if p.title:
+                    return await edit_or_reply(
+                        event, f"**Chat: `{p.title}`\n **ID:** `{p.id}`"
+                    )
+            except Exception as e:
+                LOGS.info(str(e))
+        await edit_or_reply(event, "`Rispondi a un utente o usa l'username`")
+    elif event.reply_to_msg_id:
+        await event.get_input_chat()
         r_msg = await event.get_reply_message()
         if r_msg.media:
             bot_api_file_id = pack_bot_file_id(r_msg.media)
-            await event.edit("**Chat ID**: `{}`\n**User ID**: `{}`\n**Bot API File ID**: `{}`".format(str(event.chat_id), str(r_msg.from_id), bot_api_file_id))
+            await edit_or_reply(
+                event,
+                f"**Chat ID:** `{str(event.chat_id)}`\n**User ID:**` {str(r_msg.sender_id)}`\n**File ID:** `{bot_api_file_id}`",
+            )
         else:
-            await event.edit("**Chat ID**: `{}`\n**User ID**: `{}`".format(str(event.chat_id), str(r_msg.from_id)))
+            await edit_or_reply(
+                event,
+                f"**Chat ID:** `{str(event.chat_id)}`\n**User ID:** `{str(r_msg.sender_id)}`",
+            )
     else:
-        await event.edit("**Chat ID**: `{}`".format(str(event.chat_id)))
+        await edit_or_reply(event, f"**Chat ID:** `{str(event.chat_id)}`")
