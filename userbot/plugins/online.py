b4bc20e78854a0eaea5aace571390d570bc476c7
Leoatomic <69369111+Leoatomic@users.noreply.github.com>
2021-04-05 18:16:12 +0200

    Update online.py

--- userbot/plugins/online.py
+++ userbot/plugins/online.py
@@ -1,7 +1,9 @@
+import time
 import asyncio
 from telethon import events
 from userbot import bot
 from userbot.system import dev_cmd
+from . import get_readable_time
 
 @bot.on(dev_cmd(pattern="alive", outgoing=True))
 async def _(event):
