95d73ab8abacdb3a4c5cb3c87a123943233604ac
Leoatomic <69369111+Leoatomic@users.noreply.github.com>
2021-04-05 17:18:13 +0200

    Update online.py

--- userbot/plugins/online.py
+++ userbot/plugins/online.py
@@ -7,5 +7,4 @@ from userbot.system import dev_cmd
 async def _(event):
     if event.fwd_from:
         return
-    uptime = await get_readable_time((time.time() - StartTime))
     await event.edit(f"✅ **Userbot Online** ✅\n\n • 🗃 **Database:** `Working` \n • 🪐 **AtomicUserbot Version:** `4.6` \n • 🐍 **Python Version:** `3.9.2` \n • 📚 **Telethon Version:** `1.21.1`")
