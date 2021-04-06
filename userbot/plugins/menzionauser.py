bc700be7a48c476eaebb0eac4e674812f12fe802
Leoatomic <69369111+Leoatomic@users.noreply.github.com>
2021-04-06 11:09:20 +0200

    Update menzionauser.py

--- userbot/plugins/menzionauser.py
+++ userbot/plugins/menzionauser.py
@@ -4,31 +4,18 @@ from userbot.system import dev_cmd
 
 @bot.on(dev_cmd("menziona (.*)"))
 async def _(event):
-    if event.fwd_from:
-        return
-    input_str = event.pattern_match.group(1)
-    reply_to_id = await reply_id(event)
-    if event.reply_to_msg_id:
-        reply_msg = await event.get_reply_message()
-        u = reply_msg.sender_id
-    else:
-        user, input_str = input_str.split(" ", 1)
-        try:
-            u = int(user)
-        except ValueError:
-            try:
-                u = await event.client.get_entity(user)
-            except ValueError:
-                await event.delete()
-                return
-            u = int(u.id)
-        except Exception:
-            await event.delete()
-            return
-    await event.delete()
-    await event.client.send_message(
-        event.chat_id,
-        f"<a href='tg://user?id={u}'>{input_str}</a>",
-        parse_mode="HTML",
-        reply_to=reply_to_id,
-    )
+	if event.fwd_from:
+		return	
+	input_str = event.pattern_match.group(1)
+	if event.reply_to_msg_id:
+		previous_message = await event.get_reply_message()
+		if previous_message.forward:
+			replied_user = previous_message.forward.from_id
+		else:
+			replied_user = previous_message.from_id
+	else:
+		await event.edit("rispondi ad un messaggio")
+	user_id = replied_user
+	caption = """<a href='tg://user?id={}'>{}</a>""".format(user_id)
+	await event.edit(caption, parse_mode="HTML")
+
