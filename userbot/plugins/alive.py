76a6e289c4544091feb08d69b38bcb15075f90cf
Leoatomic <69369111+Leoatomic@users.noreply.github.com>
2021-04-06 20:47:33 +0200

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -5,31 +5,23 @@ from telethon.tl.types import ChannelParticipantsAdmins
 from platform import uname
 from userbot import ALIVE_NAME, bot, versions
 from userbot.system import dev_cmd
+import time
+from . import StartTime, get_readable_time
 
 from datetime import datetime
-from userbot.system import command
+
 
 @bot.on(dev_cmd(pattern="alive", outgoing=True))
 async def _(event):
     if event.fwd_from:
         return
     start = datetime.now()
-    await event.edit(f"ㅤ  [✅](tg://user?id=845549379) **Userbot Online** \n\n **《 🗃 Database:** `Working` \n **《 🪐 AtomicUserbot Version:** `1.0` \n **《 🐍 Python Version:** `3.9.2`\n **《 📚 Telethon Version:** `1.21.1` \n **《 📶 Ping:** `Calcolo...`")
+    await event.edit(f"ㅤ  [✅](tg://user?id=845549379) **Userbot Online** \n\n **《 🗃 Database:** `Working` \n **《 🪐 AtomicUserbot Version:** `1.0` \n **《 🐍 Python Version:** `3.9.2`\n **《 📚 Telethon Version:** `1.21.1` \n **《 📶 Ping:** `Calcolo...` \n**《 📶 Attivo da:** `{uptime}`")
     end = datetime.now()
     ms = (end - start).microseconds / 1000
-    await event.edit(f"ㅤ  [✅](tg://user?id=845549379) **Userbot Online** \n\n **《 🗃 Database:** `Working` \n **《 🪐 AtomicUserbot Version:** `1.0` \n **《 🐍 Python Version:** `3.9.2`\n **《 📚 Telethon Version:** `1.21.1` \n **《 📶 Ping:** `{ms}`")
+    await event.edit(f"ㅤ  [✅](tg://user?id=845549379) **Userbot Online** \n\n **《 🗃 Database:** `Working` \n **《 🪐 AtomicUserbot Version:** `1.0` \n **《 🐍 Python Version:** `3.9.2`\n **《 📚 Telethon Version:** `1.21.1` \n **《 📶 Ping:** `{ms}`  \n**《 📶 Attivo da:** `{uptime}`")
 
 @bot.on(dev_cmd(pattern=f"on", outgoing=True))
 async def amireallyalive(on):
     """ For .alive command, check if the bot is running. """
     await on.edit("**Online** [✔️](tg://user?id=845549379)")
-
-@command(pattern="^.ping")
-async def _(event):
-    if event.fwd_from:
-        return
-    start = datetime.now()
-    await event.edit("Pong!🏓")
-    end = datetime.now()
-    ms = (end - start).microseconds / 1000
-    await event.edit("Pong!🏓\n{}".format(ms))
