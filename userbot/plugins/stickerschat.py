f56dc741188a4438832eaaa4157fad3ae71e1a61
Leoatomic <69369111+Leoatomic@users.noreply.github.com>
2021-04-06 15:16:06 +0200

    Update stickerschat.py

--- userbot/plugins/stickerschat.py
+++ userbot/plugins/stickerschat.py
@@ -6,10 +6,6 @@ from telethon.tl.functions.account import UpdateNotifySettingsRequest
 from userbot.system import dev_cmd
 from userbot import CMD_HELP, ALIVE_NAME, bot
 
-# ================= CONSTANT =================
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "100101110"
-# ============================================
-
 @bot.on(dev_cmd(pattern="ext2 ?(.*)"))
 async def _(event):
     if event.fwd_from:
@@ -19,14 +15,14 @@ async def _(event):
        return
     reply_message = await event.get_reply_message() 
     if not reply_message.text:
-       await event.edit(f"`{DEFAULTUSER}:`**Rispondi ad un user.**")
+       await event.edit(f"**Rispondi ad un utente.**")
        return
     chat = "@QuotLyBot"
     sender = reply_message.sender
     if reply_message.sender.bot:
-       await event.edit(f"`{DEFAULTUSER}:`**Rispondi a un user, no al bot.**")
+       await event.edit(f"**Rispondi a un user, no al bot.**")
        return
-    await event.edit(f"`{DEFAULTUSER}:`**Creo Stickers...**")
+    await event.edit(f"**Creo Stickers...**")
     async with event.client.conversation(chat) as conv:
           try:     
               response = conv.wait_event(events.NewMessage(incoming=True,from_users=1031952739))
@@ -36,7 +32,7 @@ async def _(event):
               await event.reply("**Please sblocca** `@QuotLyBot`")
               return
           if response.text.startswith("Hi!"):
-             await event.edit(f"`{DEFAULTUSER}:`**privacy error**")
+             await event.edit(f"**privacy error**")
           else: 
              await event.delete()
              await event.client.send_message(event.chat_id, response.message)
