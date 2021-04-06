c985911595298feced9cddc30f01cd92a2a9ee67
Leoatomic <69369111+Leoatomic@users.noreply.github.com>
2021-04-06 21:44:37 +0200

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -6,7 +6,7 @@ from platform import uname
 from userbot import ALIVE_NAME, bot, versions
 from userbot.system import dev_cmd
 import time
-from . import StartTime, get_readable_time
+from userbott import StartTime, get_readable_time
 
 from datetime import datetime
 
