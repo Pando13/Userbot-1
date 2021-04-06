a92f283053be779c126c9dc18bebd2f197cf0bec
Leoatomic <69369111+Leoatomic@users.noreply.github.com>
2021-04-06 15:26:13 +0200

    Update stickersmask.py

--- userbot/plugins/stickersmask.py
+++ userbot/plugins/stickersmask.py
@@ -23,39 +23,35 @@ from PIL import Image, ImageDraw, ImageFont
 from userbot.system import register
 from userbot import bot, CMD_HELP, ALIVE_NAME
 
-# ================= CONSTANT =================
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "100101110"
-# ============================================
-
 @register(outgoing=True, pattern="^.ext3(?: |$)(.*)")
 async def _(event):
     if event.fwd_from:
         return 
     if not event.reply_to_msg_id:
-       await event.edit(f"`{DEFAULTUSER}:`**Rispondi a foto/sticker**")
+       await event.edit(f"**Rispondi a foto/sticker**")
        return
     reply_message = await event.get_reply_message() 
     if not reply_message.media:
-       await event.edit(f"`{DEFAULTUSER}:`**Rispondi a foto/sticker**")
+       await event.edit(f"**Rispondi a foto/sticker**")
        return
     chat = "@hazmat_suit_bot"
     sender = reply_message.sender
     if reply_message.sender.bot:
-       await event.edit(f"`{DEFAULTUSER}:`**Rispondi a un user, no al bot.**")
+       await event.edit(f"**Rispondi a un utente, non al bot.**")
        return
-    await event.edit(f"`{DEFAULTUSER}:`**Creo Stickers...**")
+    await event.edit(f"**Creo Stickers...**")
     async with bot.conversation(chat) as conv:
           try:     
               response = conv.wait_event(events.NewMessage(incoming=True,from_users=905164246))
               await bot.forward_messages(chat, reply_message)
               response = await response 
           except YouBlockedUserError: 
-              await event.reply("**Please sblocca** `@hazmat_suit_bot`")
+              await event.reply("**Sblocca** `@hazmat_suit_bot`")
               return
           if response.text.startswith("Forward"):
-             await event.edit(f"`{DEFAULTUSER}:`**privacy error**")
+             await event.edit(f"**privacy error**")
           else:
           	if response.text.startswith("Select"):
-          		await event.edit(f"`{DEFAULTUSER}:`Please vai su @DrWebBot e select la lingua.") 
+          		await event.edit(f"Vai su @DrWebBot e seleziona la lingua.") 
           	else: 
           			await bot.send_file(event.chat_id, response.message.media)
