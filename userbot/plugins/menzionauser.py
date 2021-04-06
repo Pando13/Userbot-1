88285e7df64362b4b55e7acaeb87b7eb1fd1ff73
Leoatomic <69369111+Leoatomic@users.noreply.github.com>
2021-04-06 11:39:35 +0200

    Update menzionauser.py

--- userbot/plugins/menzionauser.py
+++ userbot/plugins/menzionauser.py
@@ -8,14 +8,13 @@ async def _(event):
 		return	
 	input_str = event.pattern_match.group(1)
 	if event.reply_to_msg_id:
-		previous_message = await event.get_reply_message()
-		if previous_message.forward:
-			replied_user = previous_message.forward.from_id
+		r_msg = await event.get_reply_message()
+		if r_msg.forward:
+			replied_user = r_msg.forward.from_id
 		else:
-			replied_user = previous_message.from_id
+			replied_user = r_msg.from_id
 	else:
-		await event.edit("rispondi ad un messaggio")
+		await event.edit("Rispondi ad un messaggio")
 	user_id = replied_user
-	caption = """<a href='tg://user?id={}'>{}</a>""".format(user_id, input_str)
+	caption = """<a href='tg://user?id={}'>{}</a>""".format(str(r_msg.from_id.user_id), input_str)	
 	await event.edit(caption, parse_mode="HTML")
-
