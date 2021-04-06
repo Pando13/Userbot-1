4c1acd124aca82888659f186abcdbcbaae68bde6
Leoatomic <69369111+Leoatomic@users.noreply.github.com>
2021-04-06 15:32:18 +0200

    Update sangmata.py

--- userbot/plugins/sangmata.py
+++ userbot/plugins/sangmata.py
@@ -11,38 +11,33 @@ from userbot import CMD_HELP, ALIVE_NAME, bot
 from userbot.system import dev_cmd
 
 
-# ================= CONSTANT =================
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "100101110"
-# ============================================
-
-
 @bot.on(dev_cmd(pattern=("sg ?(.*)")))
 async def _(event):
    if event.fwd_from:
       return 
    if not event.reply_to_msg_id:
-      await event.edit(f"`{DEFAULTUSER}:`**Rispondi ad un user.**")
+      await event.edit(f"**Rispondi ad un utente.**")
       return
    reply_message = await event.get_reply_message() 
    if not reply_message.text:
-      await event.edit(f"`{DEFAULTUSER}:`**Rispondi ad un user.**")
+      await event.edit(f"**Rispondi ad un utente.**")
       return
    chat = "@SangMataInfo_bot"
    sender = reply_message.sender
    if reply_message.sender.bot:
-      await event.edit(f"`{DEFAULTUSER}:`**Rispondi a un user, no al bot.**")
+      await event.edit(f"**Rispondi ad un utente, non al bot.**")
       return
-   await event.edit(f"`{DEFAULTUSER}:`**Ricerca Storico...**")
+   await event.edit(f"**Ricerca Storico...**")
    async with bot.conversation(chat) as conv:
          try:     
             response = conv.wait_event(events.NewMessage(incoming=True,from_users=461843263))
             await bot.forward_messages(chat, reply_message)
             response = await response 
          except YouBlockedUserError: 
-            await event.reply("**Please sblocca** `@Sangmatainfo_bot`")
+            await event.reply("**Sblocca** `@Sangmatainfo_bot`")
             return
          if response.text.startswith("Forward"):
-            await event.edit(f"`{DEFAULTUSER}:`**privacy error**")
+            await event.edit(f"**privacy error**")
          else: 
             await event.edit(f"{response.message.message}")
 
@@ -52,27 +47,27 @@ async def _(event):
     if event.fwd_from:
         return 
     if not event.reply_to_msg_id:
-       await event.edit(f"`{DEFAULTUSER}:`**Rispondi ad un user.**")
+       await event.edit(f"**Rispondi ad un utente.**")
        return
     reply_message = await event.get_reply_message()
     if not reply_message.text:
-       await event.edit(f"`{DEFAULTUSER}:`**Rispondi ad un user.**")
+       await event.edit(f"**Rispondi ad un utente.**")
        return
     chat = "@getidsbot"
     sender = reply_message.sender
     if reply_message.sender.bot:
-       await event.edit(f"`{DEFAULTUSER}:`**Rispondi a un user, no al bot.**")
+       await event.edit(f"**Rispondi ad un utente, non al bot.**")
        return
-    await event.edit(f"`{DEFAULTUSER}:`**Ricerca Info...**")
+    await event.edit(f"**Ricerca Info...**")
     async with bot.conversation(chat) as conv:
           try:     
               response = conv.wait_event(events.NewMessage(incoming=True,from_users=186675376))
               await bot.forward_messages(chat, reply_message)
               response = await response 
           except YouBlockedUserError: 
-              await event.reply("**Please sblocca** `@getidsbot`")
+              await event.reply("**Sblocca** `@getidsbot`")
               return
           if response.text.startswith("Forward"):
-             await event.edit(f"`{DEFAULTUSER}:`**privacy error**")
+             await event.edit(f"**privacy error**")
           else: 
              await event.edit(f"{response.message.message}")
