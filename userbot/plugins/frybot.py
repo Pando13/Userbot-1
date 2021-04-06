f6c9ec9703656f31f7e5fc0a8be069a100d9c50b
Leoatomic <69369111+Leoatomic@users.noreply.github.com>
2021-04-06 15:20:39 +0200

    Update frybot.py

--- userbot/plugins/frybot.py
+++ userbot/plugins/frybot.py
@@ -22,37 +22,32 @@ from PIL import Image, ImageDraw, ImageFont
 from userbot import bot, CMD_HELP, ALIVE_NAME
 from userbot.system import register
 
-# ================= CONSTANT =================
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "100101110"
-# ============================================
-
-
 @register(outgoing=True, pattern="^.frybot(?: |$)(.*)")
 async def _(event):
     if event.fwd_from:
         return 
     if not event.reply_to_msg_id:
-       await event.edit(f"`{DEFAULTUSER}:`**Rispondi a foto/sticker**")
+       await event.edit(f"**Rispondi a foto/sticker**")
        return
     reply_message = await event.get_reply_message() 
     if not reply_message.media:
-       await event.edit(f"`{DEFAULTUSER}:`**Rispondi a foto/sticker**")
+       await event.edit(f"**Rispondi a foto/sticker**")
        return
     chat = "@image_deepfrybot"
     sender = reply_message.sender
     if reply_message.sender.bot:
-       await event.edit(f"`{DEFAULTUSER}:`**Rispondi a un user, no al bot.**")
+       await event.edit(f"**Rispondi a un utente, non al bot.**")
        return
-    await event.edit(f"`{DEFAULTUSER}:`**Creo img/sticker...**")
+    await event.edit(f"**Creo img/sticker...**")
     async with bot.conversation(chat) as conv:
           try:     
               response = conv.wait_event(events.NewMessage(incoming=True,from_users=432858024))
               await bot.forward_messages(chat, reply_message)
               response = await response 
           except YouBlockedUserError: 
-              await event.reply("**Please sblocca `@image_deepfrybot` **")
+              await event.reply("**Sblocca `@image_deepfrybot` **")
               return
           if response.text.startswith("Forward"):
-             await event.edit(f"`{DEFAULTUSER}:`**privacy error**")
+             await event.edit(f"**privacy error**")
           else: 
                 await bot.send_file(event.chat_id, response.message.media)
