012a7dfa37a95008ec70def8c1c92be2cd83c472
Leoatomic <69369111+Leoatomic@users.noreply.github.com>
2021-04-06 10:32:24 +0200

    Update get_id.py

--- userbot/plugins/get_id.py
+++ userbot/plugins/get_id.py
@@ -11,13 +11,12 @@ async def _(event):
     if event.fwd_from:
         return
     if event.reply_to_msg_id:
-        user_id = replied_user.user.id
         chat = await event.get_input_chat()
         r_msg = await event.get_reply_message()
         if r_msg.media:
             bot_api_file_id = pack_bot_file_id(r_msg.media)
-            await event.edit("**Chat ID**: `{}`\n**User ID**: `{}`\n**Bot API File ID**: `{}`".format(str(event.chat_id), str(r_msg.from_id), bot_api_file_id))
+            await event.edit("**Chat ID:** `{}` \n **User ID:** `{}` \n **Bot API File ID:** `{}`".format(str(event.chat_id), str(r_msg.from_id.user_id), bot_api_file_id))
         else:
-            await event.edit("**Chat ID**: `{}`\n**User ID**: `{}`".format(str(event.chat_id), str(r_msg.from_id)))
+            await event.edit("**Chat ID:** `{}` \n **User ID:** `{}`".format(str(event.chat_id), str(r_msg.from_id.user_id)))
     else:
-        await event.edit("**Chat ID**: `{}`".format(str(user_id.chat_id)))
+        await event.edit("**Chat ID:** `{}` ".format(str(event.chat_id)))
