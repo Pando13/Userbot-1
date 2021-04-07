5ac44a2068f888cbc38dbfdbf9549726cffe103f
Leoatomic <69369111+Leoatomic@users.noreply.github.com>
2021-04-07 16:43:04 +0200

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -38,10 +38,10 @@ async def _(event):
         return
     uptime = await get_readable_time((time.time() - StartTime))
     start = datetime.now()
-    await event.edit(f"ㅤ  [✅](tg://user?id=845549379) **Userbot Online** \n\n **《 🗃 Database:** `Working` \n **《 🪐 AtomicUserbot Version:** `1.0` \n **《 🐍 Python Version:** `3.9.2`\n **《 📚 Telethon Version:** `1.21.1` \n **《 📶 Ping:** `Calcolo...` \n **《 ⏳ Attivo da:** `{uptime}`")
+    await event.edit(f"[⚙️](tg://user?id=845549379) **Userbot Online** \n\n ** • 🗃 Database:** `Working` \n ** • 🪐 AtomicUserbot Version:** `1.0` \n ** • 🐍 Python Version:** `3.9.2`\n ** • 📚 Telethon Version:** `1.21.1` \n ** • 📶 Ping:** `Calcolo...` \n ** • ⏳ Attivo da:** `{uptime}`")
     end = datetime.now()
     ms = (end - start).microseconds / 1000
-    await event.edit(f"ㅤ  [✅](tg://user?id=845549379) **Userbot Online** \n\n **《 🗃 Database:** `Working` \n **《 🪐 AtomicUserbot Version:** `1.0` \n **《 🐍 Python Version:** `3.9.2`\n **《 📚 Telethon Version:** `1.21.1` \n **《 📶 Ping:** `{ms}` \n **《 ⏳ Attivo da:** `{uptime}`")
+    await event.edit(f"[⚙️](tg://user?id=845549379) **Userbot Online** \n\n ** • 🗃 Database:** `Working` \n ** • 🪐 AtomicUserbot Version:** `1.0` \n ** • 🐍 Python Version:** `3.9.2`\n ** • 📚 Telethon Version:** `1.21.1` \n ** • 📶 Ping:** `{ms}` \n ** • ⏳ Attivo da:** `{uptime}`")
 
 @bot.on(dev_cmd(pattern=f"on", outgoing=True))
 async def amireallyalive(on):
