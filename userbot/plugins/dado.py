8a74107acd92de40d665fc321184523ae59b9f60
Leoatomic <69369111+Leoatomic@users.noreply.github.com>
2021-04-08 20:55:41 +0200

    Create dado.py

---
+++ userbot/plugins/dado.py
@@ -0,0 +1,30 @@
+import asyncio
+import random
+import re
+import time
+import requests
+
+from collections import deque
+
+from telethon.tl.functions.users import GetFullUserRequest
+from telethon.tl.types import MessageEntityMentionName
+
+from userbot import CMD_HELP
+from userbot.system import register
+
+
+
+dado = [
+    "**È uscito:** 1 🎲",
+    "**È uscito:** 2 🎲",
+    "**È uscito:** 3 🎲",
+    "**È uscito:** 4 🎲",
+    "**È uscito:** 5 🎲",
+    "**È uscito:** 6 🎲",
+
+
+@register(outgoing=True, pattern="^.dado")
+async def dado(e):
+    """Tira un dado!"""
+    if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
+        await e.edit(random.choice(dado))
