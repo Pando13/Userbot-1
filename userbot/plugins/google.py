0fb41f84841ca2fbbb276328a817c888f3bcdd4e
Leoatomic <69369111+Leoatomic@users.noreply.github.com>
2021-04-11 14:34:48 +0200

    Update google.py

--- userbot/plugins/google.py
+++ userbot/plugins/google.py
@@ -9,12 +9,19 @@ from search_engine_parser import GoogleSearch
 from asyncio import sleep
 from userbot.system import register
 from telethon.tl.types import DocumentAttributeAudio
+from . import BOTLOG, BOTLOG_CHATID
+import io
+import urllib
+import requests
+
+opener = urllib.request.build_opener()
+useragent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36"
+opener.addheaders = [("User-agent", useragent)]
 
 @register(outgoing=True, pattern=r"^\.gs (.*)")
 async def gsearch(q_event):
-    """ For .google command, do a Google search. """
     match = q_event.pattern_match.group(1)
-    page = findall(r"page=\d+", match)
+    page = re.findall(r"page=\d+", match)
     try:
         page = page[0]
         page = page.replace("page=", "")
@@ -30,9 +37,14 @@ async def gsearch(q_event):
             title = gresults["titles"][i]
             link = gresults["links"][i]
             desc = gresults["descriptions"][i]
-            msg += f"[{title}]({link})\n`{desc}`\n\n"
+            msg += f"👉[{title}]({link})\n`{desc}`\n\n"
         except IndexError:
             break
-    await q_event.edit("**RICERCA:**\n`" + match + "`\n\n**RISULTATI:**\n" +
-                       msg,
-                       link_preview=False)
+    await q_event.edit(
+        "**Ricerca:**\n`" + match + "`\n\n**Risultati:**\n" + msg, link_preview=False
+    )
+    if BOTLOG:
+        await q_event.client.send_message(
+            BOTLOG_CHATID,
+            "Google Search query `" + match + "` was executed successfully",
+        )
