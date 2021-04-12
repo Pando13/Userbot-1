a52f868565bb6e15d33016dfb5ce7d977cfe9e4c
Leoatomic <69369111+Leoatomic@users.noreply.github.com>
2021-04-12 17:10:58 +0200

    Update traduttore.py

--- userbot/plugins/traduttore.py
+++ userbot/plugins/traduttore.py
@@ -2,48 +2,47 @@
 Commands:
 .tr LanguageCode as reply to a message
 .tr LangaugeCode | text to translate"""
-import asyncio
-import os
-import re
 
-import requests
-
-from userbot.uniborgConfig import Config
 import emoji
 from googletrans import Translator
 from userbot import bot
 from userbot.system import dev_cmd
-from asyncio import sleep
-from emoji import get_emoji_regexp
-from telethon import events
+
 
 @bot.on(dev_cmd("tr ?(.*)"))
 async def _(event):
     if event.fwd_from:
         return
     if "trim" in event.raw_text:
+        # https://t.me/c/1220993104/192075
         return
     input_str = event.pattern_match.group(1)
     if event.reply_to_msg_id:
         previous_message = await event.get_reply_message()
         text = previous_message.message
         lan = input_str or "it"
-    elif ";" in input_str:
-        lan, text = input_str.split(";")
+    elif "|" in input_str:
+        lan, text = input_str.split("|")
     else:
-        await event.edit(event, ".tr LanguageCode` **in risposta a messaggi**")
+        await event.edit("`.tr LanguageCode` **in risposta a messaggi**")
         return
-    text = deEmojify(text.strip())
+    text = emoji.demojize(text.strip())
     lan = lan.strip()
-    Translator()
+    translator = Translator()
     try:
-        translated = await getTranslate(text, dest=lan)
+        translated = translator.translate(text, dest=lan)
         after_tr_text = translated.text
-        output_str = f"**Tradotto da {LANGUAGES[translated.src].title()} a {LANGUAGES[lan].title()}**\
-                \n`{after_tr_text}`"
-        await edit_or_reply(event, output_str)
+        # TODO: emojify the :
+        # either here, or before translation
+        output_str = """**TRADUZIONE** da {} a {}
+{}""".format(
+            translated.src,
+            lan,
+            after_tr_text
+        )
+        await event.edit(output_str)
     except Exception as exc:
-        await event.edit(event, str(exc)
+        await event.edit(str(exc))
         
 def getTranslate(text, **kwargs):
     translator = Translator()
@@ -55,8 +54,3 @@ def getTranslate(text, **kwargs):
             translator = Translator()
             await sleep(0.1)
     return result
-
-def deEmojify(inputString: str) -> str:
-    """Remove emojis and other non-safe characters from string"""
-    return get_emoji_regexp().sub("", inputString)
-
