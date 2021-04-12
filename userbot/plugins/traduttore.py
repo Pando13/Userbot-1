2196d5033a6d404afe697de7979e378d2d4f1de1
Leoatomic <69369111+Leoatomic@users.noreply.github.com>
2021-04-12 15:50:25 +0200

    Update traduttore.py

--- userbot/plugins/traduttore.py
+++ userbot/plugins/traduttore.py
@@ -30,7 +30,7 @@ async def _(event):
     lan = lan.strip()
     translator = Translator()
     try:
-        translated = translator.getTranslate(text, dest=lan)
+        translated = await getTranslate(text, dest=lan)
         after_tr_text = translated.text
         # TODO: emojify the :
         # either here, or before translation
