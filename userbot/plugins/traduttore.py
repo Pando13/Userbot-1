e1ce88feb81af45c392568799bcdf0d5db2f20ed
Leoatomic <69369111+Leoatomic@users.noreply.github.com>
2021-04-12 16:23:27 +0200

    Update traduttore.py

--- userbot/plugins/traduttore.py
+++ userbot/plugins/traduttore.py
@@ -14,35 +14,28 @@ async def _(event):
     if event.fwd_from:
         return
     if "trim" in event.raw_text:
-        # https://t.me/c/1220993104/192075
         return
     input_str = event.pattern_match.group(1)
     if event.reply_to_msg_id:
         previous_message = await event.get_reply_message()
         text = previous_message.message
-        lan = input_str or "it"
-    elif "|" in input_str:
-        lan, text = input_str.split("|")
+        lan = input_str or "en"
+    elif ";" in input_str:
+        lan, text = input_str.split(";")
     else:
-        await event.edit("`.tr LanguageCode` **in risposta a messaggi**")
+        await edit_delete(event, ".tr LanguageCode` **in risposta a messaggi**", time=5)
         return
-    text = emoji.demojize(text.strip())
+    text = deEmojify(text.strip())
     lan = lan.strip()
-    translator = Translator()
+    Translator()
     try:
         translated = await getTranslate(text, dest=lan)
         after_tr_text = translated.text
-        # TODO: emojify the :
-        # either here, or before translation
-        output_str = """**TRADUZIONE** da {} a {}
-{}""".format(
-            translated.src,
-            lan,
-            after_tr_text
-        )
-        await event.edit(output_str)
+        output_str = f"**Tradotto da {LANGUAGES[translated.src].title()} a {LANGUAGES[lan].title()}**\
+                \n`{after_tr_text}`"
+        await edit_or_reply(event, output_str)
     except Exception as exc:
-        await event.edit(str(exc))
+        await edit_delete(event, str(exc), time=5)
         
        
 async def getTranslate(text, **kwargs):
@@ -55,3 +48,8 @@ async def getTranslate(text, **kwargs):
             translator = Translator()
             await sleep(0.1)
     return result
+
+def deEmojify(inputString: str) -> str:
+    """Remove emojis and other non-safe characters from string"""
+    return get_emoji_regexp().sub("", inputString)
+
