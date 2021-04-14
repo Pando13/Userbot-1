0430fb14b92c2105f77f14da7b06977f4ad690d3
Leoatomic <69369111+Leoatomic@users.noreply.github.com>
2021-04-14 19:42:02 +0200

    Update infogroup.py

--- userbot/plugins/infogroup.py
+++ userbot/plugins/infogroup.py
@@ -8,29 +8,33 @@ from telethon.tl.functions.account import UpdateNotifySettingsRequest
 import asyncio
 from userbot import CMD_HELP, ALIVE_NAME, bot
 from userbot.system import dev_cmd
-
+message_sent = 0
 
 @bot.on(dev_cmd(pattern=("ginfo ?(.*)")))
 async def _(event):
+  global reply_message
+   if reply_message <1:
+      reply_message += 1
+      return
    if event.fwd_from:
       return 
    if not event.reply_to_msg_id:
       await event.edit(f"**Rispondi ad un utente.**")
       return
-   reply_messages = await event.get_reply_message() 
-   if not reply_messages.text:
+   reply_message = await event.get_reply_message() 
+   if not reply_message.text:
       await event.edit(f"**Rispondi ad un utente.**")
       return
    chat = "@tgscanrobot"
-   sender = reply_messages.sender
-   if reply_messages.sender.bot:
+   sender = reply_message.sender
+   if reply_message.sender.bot:
       await event.edit(f"**Rispondi ad un utente, non al bot.**")
       return
    await event.edit(f"**Ricerca info...**")
    async with bot.conversation(chat) as conv:
          try:     
             response = conv.wait_event(events.NewMessage(incoming=True,from_users=1557162396))
-            await bot.forward_messages(chat, reply_messages)
+            await bot.forward_message(chat, reply_message)
             response = await response 
          except YouBlockedUserError: 
             await event.reply("**Sblocca** `@tgscanrobot`")
