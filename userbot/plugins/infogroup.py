9b96513da6e4ca9e097d976a2257a61c53299c52
Leoatomic <69369111+Leoatomic@users.noreply.github.com>
2021-04-14 19:15:28 +0200

    Update infogroup.py

--- userbot/plugins/infogroup.py
+++ userbot/plugins/infogroup.py
@@ -30,8 +30,7 @@ async def _(event):
    async with bot.conversation(chat) as conv:
          try:     
             response = conv.wait_event(events.NewMessage(incoming=True,from_users=1557162396))
-            await bot.forward_messages(chat)
-            await bot.forward_messages(reply_message)
+            await bot.forward_messages(chat, reply_messages)
             response = await response 
          except YouBlockedUserError: 
             await event.reply("**Sblocca** `@tgscanrobot`")
