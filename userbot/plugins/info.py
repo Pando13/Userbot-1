e548d9a5607420af268ebd1a2e3b1198b0fbfc62
Leoatomic <69369111+Leoatomic@users.noreply.github.com>
2021-05-11 16:12:17 +0200

    Update info.py

--- userbot/plugins/info.py
+++ userbot/plugins/info.py
@@ -231,7 +231,7 @@ async def _(event):
         status = ("Ultimo accesso entro una settimana")
     if status == ("UserStatusLastMonth()"):
         status = ("Ultimo accesso entro un mese")
-        else ("Online")
+        else: ("Online")
     verified = replied_user.user.verified
     verified = "{}".format(verified) if verified else ("No")
     is_bot = replied_user.user.bot
