cc2a98cc1a1b4a94fb12f7183319adad3784da29
Leoatomic <69369111+Leoatomic@users.noreply.github.com>
2021-06-03 12:30:27 +0200

    Update memes.py

--- userbot/plugins/memes.py
+++ userbot/plugins/memes.py
@@ -153,8 +153,8 @@ async def bluetext(bte):
     if not bte.text[0].isalpha() and bte.text[0] not in ("/", "#", "@", "!"):
         if await bte.get_reply_message():
             await bte.edit(
-                "`*TESTO BLU! DEVO CLICCARE!*.`\n"
-                "`Sei uno come uno stupido animale attratto dai colori?`"
+                "`/TESTO /BLU /DEVO /CLICCARE`\n"
+                "`/SONO /UNO /STUPIDO /ANIMALE /ATTRATTO /DAI /COLORI`"
             )
 
 
