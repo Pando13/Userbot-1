9cc81ee45a0b2c0ad4633b90dd420f5da1c17b89
Leoatomic <69369111+Leoatomic@users.noreply.github.com>
2021-06-04 21:07:04 +0200

    Create zombies.py

---
+++ userbot/plugins/zombies.py
@@ -0,0 +1,125 @@
+"""Cmd= `.zombie`
+Uso: Cerca account eliminati nei gruppi.
+con ".zombies clean" rimuovi gli account eliminati."""
+
+from telethon import events
+from userbot.utils import admin_cmd
+#
+from asyncio import sleep
+from os import remove
+
+from telethon.errors import (BadRequestError, ChatAdminRequiredError,
+                             ImageProcessFailedError, PhotoCropSizeSmallError,
+                             UserAdminInvalidError)
+from telethon.errors.rpcerrorlist import (UserIdInvalidError,
+                                          MessageTooLongError)
+from telethon.tl.functions.channels import (EditAdminRequest,
+                                            EditBannedRequest,
+                                            EditPhotoRequest)
+from telethon.tl.types import (ChannelParticipantsAdmins, ChatAdminRights,
+                               ChatBannedRights, MessageEntityMentionName,
+                               MessageMediaPhoto)
+
+
+# =================== CONSTANT ===================
+
+BANNED_RIGHTS = ChatBannedRights(
+    until_date=None,
+    view_messages=True,
+    send_messages=True,
+    send_media=True,
+    send_stickers=True,
+    send_gifs=True,
+    send_games=True,
+    send_inline=True,
+    embed_links=True,
+)
+
+UNBAN_RIGHTS = ChatBannedRights(
+    until_date=None,
+    send_messages=None,
+    send_media=None,
+    send_stickers=None,
+    send_gifs=None,
+    send_games=None,
+    send_inline=None,
+    embed_links=None,
+)
+
+MUTE_RIGHTS = ChatBannedRights(until_date=None, send_messages=True)
+
+UNMUTE_RIGHTS = ChatBannedRights(until_date=None, send_messages=False)
+# ================================================
+
+
+
+@bot.on(admin_cmd(pattern=f"zombies", allow_sudo=True))
+@bot.on(events.NewMessage(pattern="^.zombies(?: |$)(.*)", outgoing=True))
+async def rm_deletedacc(show):
+    """ For .zombies command, list all the ghost/deleted/zombie accounts in a chat. """
+
+    con = show.pattern_match.group(1).lower()
+    del_u = 0
+    del_status = "Non ho trovato account eliminati"
+
+    if con != "clean":
+        await show.edit("Cerco zombie")
+        async for user in show.client.iter_participants(show.chat_id):
+
+            if user.deleted:
+                del_u += 1
+                await sleep(1)
+        if del_u > 0:
+            del_status = f"Account eliminati: **{del_u}**,\
+            \nRimuovili con `.zombies clean`"
+        await show.edit(del_status)
+        return
+
+    # Here laying the sanity check
+    chat = await show.get_chat()
+    admin = chat.admin_rights
+    creator = chat.creator
+
+    # Well
+    if not admin and not creator:
+        await show.edit("Non sono admin")
+        return
+
+    await show.edit("Sto eliminando..")
+    del_u = 0
+    del_a = 0
+
+    async for user in show.client.iter_participants(show.chat_id):
+        if user.deleted:
+            try:
+                await show.client(
+                    EditBannedRequest(show.chat_id, user.id, BANNED_RIGHTS))
+            except ChatAdminRequiredError:
+                await show.edit("Non ho il permesso di bannare")
+                return
+            except UserAdminInvalidError:
+                del_u -= 1
+                del_a += 1
+            await show.client(
+                EditBannedRequest(show.chat_id, user.id, UNBAN_RIGHTS))
+            del_u += 1
+
+
+    if del_u > 0:
+        del_status = f"Rimossi **{del_u}** accoun6t eliminati"
+
+    if del_a > 0:
+        del_status = f"Rimossi **{del_u}** accoun6t eliminati \
+        \n**{del_a}** account eliminati degli admin non rimossi"
+
+
+    await show.edit(del_status)
+    await sleep(2)
+    await show.delete()
+
+
+    if Config.G_BAN_LOGGER_GROUP is not None:
+        await show.client.send_message(
+            Config.G_BAN_LOGGER_GROUP, "#CLEANUP\n"
+            f"Cleaned **{del_u}** deleted account(s) !!\
+            \nCHAT: {show.chat.title}(`{show.chat_id}`)")
