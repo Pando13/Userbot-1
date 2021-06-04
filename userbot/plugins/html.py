2f0559a710961a20873d139ccd9fa82a7b34cb53
Leoatomic <69369111+Leoatomic@users.noreply.github.com>
2021-06-04 15:36:43 +0200

    Update html.py

--- userbot/plugins/html.py
+++ userbot/plugins/html.py
@@ -1,37 +1,40 @@
+import logging
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
+                    level=logging.WARNING)
 import datetime
 from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 from telethon.tl.functions.account import UpdateNotifySettingsRequest
-from userbot.system import dev_cmd
+import asyncio
 from userbot import CMD_HELP, ALIVE_NAME, bot
+from userbot.system import dev_cmd
 
-@bot.on(dev_cmd(pattern="get_html ?(.*)"))
+@bot.on(dev_cmd(pattern=("get_html ?(.*)")))
 async def _(event):
-    if event.fwd_from:
-        return 
-    if not event.reply_to_msg_id:
-       await event.edit(f"**Rispondi ad un messaggio**")
-       return
-    reply_message = await event.get_reply_message() 
-    if not reply_message.text:
-       await event.edit(f"**Rispondi ad un messaggio**")
-       return
-    chat = "@markuptohtmlbot"
-    sender = reply_message.sender
-    if reply_message.sender.bot:
-       await event.edit(f"**Rispondi a un messaggio non al bot**")
-       return
-    await event.edit(f"**Carico HTML...**")
-    async with event.client.conversation(chat) as conv:
-          try:     
-              response = conv.wait_event(events.NewMessage(incoming=True,from_users=494105609))
-              await event.client.forward_messages(chat, reply_message)
-              response = await response 
-          except YouBlockedUserError: 
-              await event.reply("**Sblocca** `@markuptohtmlbot`")
-              return
-          if response.text.startswith("Hi!"):
-             await event.edit(f"**privacy error**")
-          else: 
-             await event.delete()
-             await event.client.send_message(event.chat_id, response.message)
+   if event.fwd_from:
+      return 
+   if not event.reply_to_msg_id:
+      await event.edit(f"**Rispondi ad un utente.**")
+      return
+   reply_message = await event.get_reply_message() 
+   if not reply_message.text:
+      await event.edit(f"**Rispondi ad un utente.**")
+      return
+   chat = "@markuptohtmlbot"
+   sender = reply_message.sender
+   if reply_message.sender.bot:
+      await event.edit(f"**Rispondi ad un utente, non al bot.**")
+      return
+   await event.edit(f"**Ricerca Storico...**")
+   async with bot.conversation(chat) as conv:
+         try:     
+            response = conv.wait_event(events.NewMessage(incoming=True,from_users=494105609))
+            await bot.forward_messages(chat, reply_message)
+            response = await response 
+         except YouBlockedUserError: 
+            await event.reply("**Sblocca** `@markuptohtmlbot`")
+            return
+         if response.text.startswith("Forward"):
+            await event.edit(f"**privacy error**")
+         else: 
+            await event.edit(f"{response.message.message}")
