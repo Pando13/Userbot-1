3ad4d8c1b39388d61a61805ef5e0b85235eff32c
Leoatomic <69369111+Leoatomic@users.noreply.github.com>
2021-06-06 16:49:35 +0200

    Create get_user.py

---
+++ userbot/plugins/get_user.py
@@ -0,0 +1,144 @@
+"""Get Telegram Profile and other information
+Syntax:
+.dc
+.chatinfo
+.info @username
+.stats
+"""
+
+import asyncio
+import time
+import sys
+import html
+
+from datetime import datetime
+from emoji import emojize
+from math import sqrt
+from telethon.tl.functions.channels import GetFullChannelRequest, GetParticipantsRequest
+from telethon.tl.functions.photos import GetUserPhotosRequest
+from telethon.tl.functions.users import GetFullUserRequest
+from telethon.tl.functions.messages import GetHistoryRequest, CheckChatInviteRequest, GetFullChatRequest
+from telethon.tl.types import User, Chat, Channel
+from telethon.tl.types import MessageActionChannelMigrateFrom, MessageEntityMentionName, ChannelParticipantsAdmins 
+from telethon.errors import (ChannelInvalidError, ChannelPrivateError, ChannelPublicGroupNaError, InviteHashEmptyError, InviteHashExpiredError, InviteHashInvalidError)
+from telethon.utils import get_input_location
+from telethon import events, functions
+from telethon.events import NewMessage
+from telethon.tl.custom import Dialog
+from userbot import CMD_HELP, bot, ALIVE_NAME
+from userbot.system import dev_cmd, register, errors_handler
+
+# ================= CONSTANT =================
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "@AtomicUserbot"
+# ============================================
+
+
+@bot.on(dev_cmd("info ?(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    replied_user, error_i_a = await get_full_user(event)
+    if replied_user is None:
+        await event.edit(str(error_i_a))
+        return False
+    user_id = replied_user.user.id
+    # some people have weird HTML in their names
+    first_name = html.escape(replied_user.user.first_name)
+    # https://stackoverflow.com/a/5072031/4723940
+    # some Deleted Accounts do not have first_name
+    if first_name is not None:
+        # some weird people (like me) have more than 4096 characters in their names
+        first_name = first_name.replace("\u2060", "")
+    username = replied_user.user.username
+    username = "@{}".format(username) if username else (" ")
+    data = time.strftime("%d/%m/%Y")
+    ora = time.strftime("%H:%M:%S")
+    try:
+        dc_id, location = get_input_location(replied_user.profile_photo)
+    except Exception as e:
+        dc_id = "N.D."
+        location = str(e)
+    caption = """
+🧑‍💻 <a href='tg://user?id={}'>{}</a> 
+🆔 <code>{}</code>
+🌐 {}
+🔖 {}
+⏳ <i>{} {}</i>
+""".format(
+        user_id,
+        first_name,
+        user_id,
+        username,
+        dc_id,
+        data,
+        ora,
+    )
+    message_id_to_reply = event.message.reply_to_msg_id
+    if not message_id_to_reply:
+        message_id_to_reply = event.message.id
+    await bot.send_message(
+        event.chat_id,
+        caption,
+        reply_to=message_id_to_reply,
+        parse_mode="HTML",
+        file=replied_user.profile_photo,
+        force_document=False,
+        silent=True
+    )
+    await event.delete()
+
+
+async def get_full_user(event):
+    if event.reply_to_msg_id:
+        previous_message = await event.get_reply_message()
+        if previous_message.forward:
+            replied_user = await event.client(
+                GetFullUserRequest(
+                    previous_message.forward.from_id or previous_message.forward.channel_id
+                )
+            )
+            return replied_user, None
+        else:
+            replied_user = await event.client(
+                GetFullUserRequest(
+                    previous_message.from_id
+                )
+            )
+            return replied_user, None
+    else:
+        input_str = None
+        try:
+            input_str = event.pattern_match.group(1)
+        except IndexError as e:
+            return None, e
+        if event.message.entities is not None:
+            mention_entity = event.message.entities
+            probable_user_mention_entity = mention_entity[0]
+            if isinstance(probable_user_mention_entity, MessageEntityMentionName):
+                user_id = probable_user_mention_entity.user_id
+                replied_user = await event.client(GetFullUserRequest(user_id))
+                return replied_user, None
+            else:
+                try:
+                    user_object = await event.client.get_entity(input_str)
+                    user_id = user_object.id
+                    replied_user = await event.client(GetFullUserRequest(user_id))
+                    return replied_user, None
+                except Exception as e:
+                    return None, e
+        elif event.is_private:
+            try:
+                user_id = event.chat_id
+                replied_user = await event.client(GetFullUserRequest(user_id))
+                return replied_user, None
+            except Exception as e:
+                return None, e
+        else:
+            try:
+                user_object = await event.client.get_entity(int(input_str))
+                user_id = user_object.id
+                replied_user = await event.client(GetFullUserRequest(user_id))
+                return replied_user, None
+            except Exception as e:
+                return None, e
+
