e96ae079dedc5c2903ddceeb6e3befc9de2a27c5
Leoatomic <69369111+Leoatomic@users.noreply.github.com>
2021-06-06 23:29:00 +0200

    Update blocca.py

--- userbot/plugins/blocca.py
+++ userbot/plugins/blocca.py
@@ -9,36 +9,32 @@ from userbot.system import dev_cmd, command
 
 @command(pattern="^.block ?(.*)")
 async def block_p_m(event):
-        if event.fwd_from:
-            return
-        replied_user, error_i_a = await get_full_user(event)
-        if replied_user is None:
-            await event.edit(str(error_i_a))
-            return False
-        user = await event.get_chat()
-        firstname = replied_user.user.first_name
-        reason = event.pattern_match.group(1)
         if event.is_private:
-          await event.client(functions.contacts.BlockRequest(user.id))
-          await event.edit("**Sei stato bloccato, non puoi inviarmi messaggi**[{}](tg://user?id={})".format(firstname, user.id))
+        user = await event.get_chat()
+    else:
+        user, reason = await get_full_user(event)
+        if not user:
+            return
+    if not reason:
+        reason = "Not Mentioned."
+     await event.client(functions.contacts.BlockRequest(user.id))
+     await event.edit("**Sei stato bloccato, non puoi inviarmi messaggi**[{}](tg://user?id={})".format(firstname, user.id))
                 
                 
                 
                 
 @command(pattern="^.unblock ?(.*)")
 async def unblock_p_m(event):
-        if event.fwd_from:
-            return
-        replied_user, error_i_a = await get_full_user(event)
-        if replied_user is None:
-            await event.edit(str(error_i_a))
-            return False
-        user = await event.get_chat()
-        firstname = replied_user.user.first_name
-        reason = event.pattern_match.group(1)
         if event.is_private:
-          await event.client(functions.contacts.UnblockRequest(user.id))
-          await event.edit("**Sei stato sbloccato, puoi inviarmi messaggi**[{}](tg://user?id={})".format(firstname, user.id))
+        user = await event.get_chat()
+    else:
+        user, reason = await get_full_user(event)
+        if not user:
+            return
+    if not reason:
+        reason = "Not Mentioned."
+    await event.client(functions.contacts.UnblockRequest(user.id))
+    await event.edit("**Sei stato sbloccato, puoi inviarmi messaggi**[{}](tg://user?id={})".format(firstname, user.id))
                 
 async def get_full_user(event):
     if event.reply_to_msg_id:
