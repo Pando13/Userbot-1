e15b5b4a967b284cb8b9dd7b92b076eb9ab8cc46
Leoatomic <69369111+Leoatomic@users.noreply.github.com>
2021-06-06 22:20:47 +0200

    Create blocca.py

---
+++ userbot/plugins/blocca.py
@@ -0,0 +1,38 @@
+import asyncio
+import io
+
+from telethon.tl.functions.users import GetFullUserRequest
+from telethon import events, errors, functions, types
+import userbot.plugins.sql_helper.pmpermit_sql as pmpermit_sql
+from userbot import ALIVE_NAME, LESS_SPAMMY, bot
+from userbot.system import dev_cmd, command
+
+
+
+@command(pattern="^.block ?(.*)")
+    async def block_p_m(event):
+        if event.fwd_from:
+            return
+        replied_user = await event.client(GetFullUserRequest(event.chat_id))
+        firstname = replied_user.user.first_name
+        reason = event.pattern_match.group(1)
+        chat = await event.get_chat()
+        if event.is_private:
+          await event.client(functions.contacts.BlockRequest(chat.id))
+          await event.edit("**Sei stato bloccato, non puoi inviarmi messaggi**[{}](tg://user?id={})".format(firstname, chat.id))
+                
+                
+                
+                
+@command(pattern="^.unblock ?(.*)")
+    async def unblock_p_m(event):
+        if event.fwd_from:
+            return
+        replied_user = await event.client(GetFullUserRequest(event.chat_id))
+        firstname = replied_user.user.first_name
+        reason = event.pattern_match.group(1)
+        chat = await event.get_chat()
+        if event.is_private:
+          await event.client(functions.contacts.UnblockRequest(user.id))
+          await event.edit("**Sei stato sbloccato, puoi inviarmi messaggi**[{}](tg://user?id={})".format(firstname, chat.id))
+                
