89504054b8c1ddbf0dd059a4953336b2dd65d3b1
Leoatomic <69369111+Leoatomic@users.noreply.github.com>
2021-06-07 15:59:21 +0200

    Update blocca.py

--- userbot/plugins/blocca.py
+++ userbot/plugins/blocca.py
@@ -11,32 +11,29 @@ from userbot.system import dev_cmd, command
 @command(pattern="^.block ?(.*)")
 async def block_p_m(event):
     if event.fwd_from:
-            return
-        replied_user = await event.client(GetFullUserRequest(event.chat_id))
-        firstname = replied_user.user.first_name
-        reason = event.pattern_match.group(1)
-        chat = await event.get_chat()
-        if event.is_private:
-            if pmpermit_sql.is_approved(chat.id):
-                pmpermit_sql.disapprove(chat.id)
-                await event.edit("**Sei stato bloccato, non puoi inviarmi messaggi**[{}](tg://user?id={})".format(firstname, chat.id))
-                await asyncio.sleep(3)
-                await event.client(functions.contacts.BlockRequest(chat.id))
-    
-        
-
+        return
+    replied_user = await event.client(GetFullUserRequest(event.chat_id))
+    firstname = replied_user.user.first_name
+    reason = event.pattern_match.group(1)
+    chat = await event.get_chat()
+    if event.is_private:
+        if pmpermit_sql.is_approved(chat.id):
+            pmpermit_sql.disapprove(chat.id)
+            await event.edit("**Sei stato bloccato, non puoi inviarmi messaggi**[{}](tg://user?id={})".format(firstname, chat.id))
+            await event.client(functions.contacts.BlockRequest(chat.id))
+                
+            
 @command(pattern="^.unblock ?(.*)")
 async def unblock_p_m(event):
     if event.fwd_from:
-            return
-        replied_user = await event.client(GetFullUserRequest(event.chat_id))
-        firstname = replied_user.user.first_name
-        reason = event.pattern_match.group(1)
-        chat = await event.get_chat()
-        if event.is_private:
-            if pmpermit_sql.disapprove(chat.id)
-                pmpermit_sql.is_approved(chat.id):
-                await event.edit("**Sei stato sbloccato, puoi inviarmi messaggi**[{}](tg://user?id={})".format(firstname, chat.id))
-                await asyncio.sleep(3)
-                await event.client(functions.contacts.UnBlockRequest(chat.id))
+        return
+    replied_user = await event.client(GetFullUserRequest(event.chat_id))
+    firstname = replied_user.user.first_name
+    reason = event.pattern_match.group(1)
+    chat = await event.get_chat()
+    if event.is_private:
+        if pmpermit_sql.disapprove(chat.id)
+            pmpermit_sql.is_approved(chat.id):
+            await event.edit("**Sei stato sbloccato, puoi inviarmi messaggi**[{}](tg://user?id={})".format(firstname, chat.id))
+            await event.client(functions.contacts.UnBlockRequest(chat.id))
         
