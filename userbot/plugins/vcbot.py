8fe9d44c5ac448d1398b6c9c967097c5e621abc0
Leonardo <69369111+Leoatomic@users.noreply.github.com>
2021-06-21 10:29:29 +0200

    Update vcbot.py

--- userbot/plugins/vcbot.py
+++ userbot/plugins/vcbot.py
@@ -47,7 +47,7 @@ def get_arg(message):
 
 
 # jiosaavn song download
-@bot.on(dev_cmd(pattern="saavn", outgoing=True))
+@bot.on_message(dev_cmd(pattern="saavn", outgoing=True))
 async def song(client, message):
     message.chat.id
     message.from_user["id"]
@@ -84,7 +84,7 @@ async def _download_song(url):
     return song_name
 
 # deezer download by william butcher bot
-@bot.on(dev_cmd(pattern="deezer", outgoing=True))
+@bot.on_message(dev_cmd(pattern="deezer", outgoing=True))
 async def deezer(_, message):
     if len(message.command) < 2:
         await message.reply_text("Che canzone cerchi? 🧐")
@@ -107,7 +107,7 @@ async def deezer(_, message):
     os.remove(song)
     await hike.delete()
 
-@bot.on(dev_cmd(pattern="play", outgoing=True))
+@bot.on_message(dev_cmd(pattern="play", outgoing=True))
 async def play_track(client, message):
     if not message.reply_to_message or not message.reply_to_message.audio:
         return
@@ -138,7 +138,7 @@ async def play_track(client, message):
     await a.edit(f'▶️ Riproduco: **{audio.title}**')
 
 
-@bot.on(dev_cmd(pattern="stopvc", outgoing=True))
+@bot.on_message(dev_cmd(pattern="stopvc", outgoing=True))
 async def stop_playing(_, message):
     group_call = VOICE_CHATS[message.chat.id]
     group_call.stop_playout()
@@ -146,7 +146,7 @@ async def stop_playing(_, message):
     await message.reply('Chiamata terminata ❌')
 
 
-@bot.on(dev_cmd(pattern="joinvc", outgoing=True))
+@bot.on_message(dev_cmd(pattern="joinvc", outgoing=True))
 async def join_voice_chat(client, message):
     input_filename = os.path.join(
         client.workdir, DEFAULT_DOWNLOAD_DIR,
@@ -166,7 +166,7 @@ async def join_voice_chat(client, message):
     await message.reply('Partecipo alla chiamata ✅')
 
 
-@bot.on(dev_cmd(pattern="leavevc", outgoing=True))
+@bot.on_message(dev_cmd(pattern="leavevc", outgoing=True))
 async def leave_voice_chat(client, message):
     chat_id = message.chat.id
     group_call = VOICE_CHATS[chat_id]
@@ -174,8 +174,4 @@ async def leave_voice_chat(client, message):
     VOICE_CHATS.pop(chat_id, None)
     await message.reply('Esco dalla chiamata ✅')
 
-bot.start()
-print('>>> USERBOT STARTED')
-idle()
-bot.stop()
-print('\n>>> USERBOT STOPPED')
+
